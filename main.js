f319226796bba8cfad3cc7fdd7554767cedf1d26
Jordan Spencer <Jordan.S.Spencer@gmail.com>
2024-10-20 11:06:57 -0400

    initial commit, all story elements in main

---
+++ main.js
@@ -0,0 +1,38 @@
+theDeepDarkWoods = {
+  name: "the deep dark woods",
+  description:
+    "There's nothing but darkness around you, barely lit by the few stars in the sky. You have no idea where you are but you figure it's got to be better than whatever is in that creepy house. Maybe you'll find a road where you can flag down some help. You hear what you think sounds like a car in the distance.",
+};
+theFrontPorch = {
+  name: "the front porch of a house",
+  description:
+    "You don't know how you got here but you are standing outside the front door of a house that you don't recognize. You look around but see only the deep dark woods in all directions. It's cold and you're very confused. You can see that the door to the house hasn't been shut all the way and there's a faint glow of light coming from inside. It's incredibly quiet but it's impossible to know if anyone is home. It seems like its the middle of the night",
+};
+theMainRoom = { name: "the main room of the house" };
+theDownstairsBathroom = { name: "the bathroom" };
+theCloset = { name: "the closet" };
+theKitchen = { name: "the kitchen" };
+theSecondFloor = { name: "upstairs" };
+theUpstairsBathroom = { name: "the bathroom" };
+theFirstBedroom = { name: "a bedroom" };
+theSecondBedroom = { name: "a bedroom" };
+theBackyard = { name: "the backyard" };
+theShed = { name: "a shed" };
+thePumpkinRoom = { name: "the pumpkin room" };
+
+// the beginning
+choice001 =
+  "Do you open the door and step inside the house or try to find your way in the woods?";
+// choice: open the door
+result001a = "";
+// choice: walk away into the woods
+result001b = "You turn and walk away from the house, into the deep dark woods.";
+
+// location: the deep dark woods
+choice010 = "  Do you walk towards the sound or away from it?";
+
+choice015 = "";
+
+choice020 = "";
+
+choice025 = "";
